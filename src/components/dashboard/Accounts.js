331f06666676ea14bb2be0c86f5d218363ccd159
Marco Riesgo Almonacid <marcoriesgo@Marcos-MacBook-Pro-2.local>
2020-05-19 17:26:51 -0700

    dashbooard complete, moving on to categories

--- src/components/dashboard/Accounts.js
+++ src/components/dashboard/Accounts.js
@@ -2,19 +2,25 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import PlaidLinkButton from "react-plaid-link-button";
 import { connect } from "react-redux";
-import {
-  getTransactions,
-  addAccount,
-  deleteAccount
-} from "../../actions/accountActions";
+import { Link } from "react-router-dom";
+import { getTransactions, addAccount, deleteAccount} from "../../actions/accountActions";
 import { logoutUser } from "../../actions/authActions";
-import MaterialTable from "material-table"; // https://mbrn.github.io/material-table/#/
+import MaterialTable from "material-table";
+
+
+import budgetspic from "../../img/budgets.png";
+import expensespic from "../../img/expenses.png";
+import billspic from "../../img/bills.png";
+
+
 class Accounts extends Component {
+  
   componentDidMount() {
     const { accounts } = this.props;
     this.props.getTransactions(accounts);
   }
-// Add account
+
+  // Add account
   handleOnSuccess = (token, metadata) => {
     const { accounts } = this.props;
     const plaidData = {
@@ -22,9 +28,10 @@ class Accounts extends Component {
       metadata: metadata,
       accounts: accounts
     };
-this.props.addAccount(plaidData);
+    this.props.addAccount(plaidData);
   };
-// Delete account
+
+  // Delete account
   onDeleteClick = id => {
     const { accounts } = this.props;
     const accountData = {
@@ -33,27 +40,29 @@ this.props.addAccount(plaidData);
     };
     this.props.deleteAccount(accountData);
   };
-// Logout
+
+
+  // Logout
   onLogoutClick = e => {
     e.preventDefault();
     this.props.logoutUser();
   };
-render() {
+
+
+  render() {
     const { user, accounts } = this.props;
     const { transactions, transactionsLoading } = this.props.plaid;
-let accountItems = accounts.map(account => (
+    
+    let accountItems = accounts.map(account => (
       <li key={account._id} style={{ marginTop: "1rem" }}>
-        <button
-          style={{ marginRight: "1rem" }}
-          onClick={this.onDeleteClick.bind(this, account._id)}
-          className="btn btn-small btn-floating waves-effect waves-light hoverable red accent-3"
-        >
+        <button style={{ marginRight: "1rem" }} onClick={this.onDeleteClick.bind(this, account._id)} className="btn btn-small btn-floating waves-effect waves-light hoverable" id="trash-button">
           <i className="material-icons">delete</i>
         </button>
         <b>{account.institutionName}</b>
       </li>
     ));
-// Setting up data table
+
+    // Data table
     const transactionsColumns = [
       { title: "Account", field: "account" },
       { title: "Date", field: "date", type: "date", defaultSort: "desc" },
@@ -61,7 +70,8 @@ let accountItems = accounts.map(account => (
       { title: "Amount", field: "amount" },
       { title: "Category", field: "category" }
     ];
-let transactionsData = [];
+
+    let transactionsData = [];
     transactions.forEach(function(account) {
       account.transactions.forEach(function(transaction) {
         transactionsData.push({
@@ -69,49 +79,60 @@ let transactionsData = [];
           date: transaction.date,
           category: transaction.category[0],
           name: transaction.name,
-          amount: transaction.amount
+          amount: "$" + transaction.amount
         });
       });
     });
 return (
       <div className="row">
         <div className="col s12">
-          <button
-            onClick={this.onLogoutClick}
-            className="btn-flat waves-effect"
-          >
-            <i className="material-icons left">keyboard_backspace</i> Log Out
+          <button onClick={this.onLogoutClick} className="btn waves-effect waves-light hoverable" id="accounts-log-out-button">
+              Logout
           </button>
-          <h4>
-            <b>Welcome!</b>
-          </h4>
-          <p className="grey-text text-darken-1">
-            Hey there, {user.name.split(" ")[0]}
-          </p>
-          <h5>
-            <b>Linked Accounts</b>
-          </h5>
-          <p className="grey-text text-darken-1">
-            Add or remove your bank accounts below
-          </p>
+          <h4><b>Hey there, {user.name.split(" ")[0]}</b></h4>
+          <div className="categories-container">
+            <Link to="/expenses">
+                <div className="card">
+                    <div className="card-image">
+                        <img src={expensespic} alt="category"/>
+                    </div>
+                    <div className="card-content">
+                        <h5>Expenses</h5>
+                    </div>
+                </div>
+            </Link>
+            <Link to="/budgets">
+                <div className="card">
+                    <div className="card-image">
+                        <img src={budgetspic} alt="category"/>
+                    </div>
+                    <div className="card-content">
+                        <h5>Budgets</h5>
+                    </div>
+                </div>
+            </Link>
+            <Link to="/bills">
+                <div className="card">
+                    <div className="card-image">
+                        <img src={billspic} alt="category"/>
+                    </div>
+                    <div className="card-content">
+                        <h5>Bills</h5>
+                    </div>
+                </div>
+            </Link>
+          </div>
+          <hr />
+          <h5><b>Linked Accounts</b></h5>
+          <p className="grey-text text-darken-1">Add or remove your bank accounts below</p>
           <ul>{accountItems}</ul>
           <PlaidLinkButton
-            buttonProps={{
-              className:
-                "btn btn-large waves-effect waves-light hoverable blue accent-3 main-btn"
-            }}
-            plaidLinkProps={{
-              clientName: "Electrum",
-              key: "540d604a54c1be6953e144f794eb33",
-              env: "sandbox",
-              product: ["transactions"],
-              onSuccess: this.handleOnSuccess
-            }}
-            onScriptLoad={() => this.setState({ loaded: true })}
-          >
+            buttonProps={{ className: "btn btn-large waves-effect waves-light hoverable blue accent-3 main-btn" }}
+            plaidLinkProps={{ clientName: "Electrum", key: "540d604a54c1be6953e144f794eb33", env: "sandbox", product: ["transactions"], onSuccess: this.handleOnSuccess }}
+            onScriptLoad={() => this.setState({ loaded: true })}>
             Add Account
           </PlaidLinkButton>
-          <hr style={{ marginTop: "2rem", opacity: ".2" }} />
+          <hr style={{ marginTop: "2rem" }} />
           <h5>
             <b>Transactions</b>
           </h5>
